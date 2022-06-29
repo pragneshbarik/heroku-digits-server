3e4ed5ddb453ae4a7f6704d9213146fe99a3ffcf
pragneshbarik <barikpragnesh@gmail.com>
2022-06-29 08:46:33 +0530

    Made some changes

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import joblib
 import json
 import os
 
+port = int(os.environ.get('PORT', 5000))
+
+
 
 digits_dict = {
     0 : "Zero",
@@ -29,21 +32,24 @@ model_address = cwd + "\model.sav"
 print (model_address)
 model = joblib.load(model_address)
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def predictor():
-    data = (request.get_json()["data"])
-    array = data[1:len(data)-1].split(",")
-    X = [int(x) for x in array]
-    X = np.array(X)
-    X = X.reshape(14, 14)
-    X = np.transpose(X)
-    X_scaled_2x = np.kron(X, np.ones((2,2)))
-    
-    to_predict = X_scaled_2x.reshape((-1, 28*28))
-    
-    prediction  = model.predict(to_predict)
-    to_send = digits_dict[prediction[0]]
-    return to_send
+    if(request.method == 'GET') :
+        return "Digits API"
+    elif (request.method == 'POST') :
+        data = (request.get_json()["data"])
+        array = data[1:len(data)-1].split(",")
+        X = [int(x) for x in array]
+        X = np.array(X)
+        X = X.reshape(14, 14)
+        X = np.transpose(X)
+        X_scaled_2x = np.kron(X, np.ones((2,2)))
+        
+        to_predict = X_scaled_2x.reshape((-1, 28*28))
+        
+        prediction  = model.predict(to_predict)
+        to_send = digits_dict[prediction[0]]
+        return to_send
 
 if __name__=="__main__" :
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
